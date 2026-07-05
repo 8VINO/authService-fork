12a4469eaa09d92d1704b8f830c96f9b33097e5e
8VINO <viniciussouto1008@gmail.com>
2026-07-05 03:43:29 -0300

    feat: adicionar variavel de abiente para deploy

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 
 const allowedOrigins = [
+    process.env.FRONTEND_URL,
     "http://localhost:5173",
     "http://localhost:3000"
 ];
